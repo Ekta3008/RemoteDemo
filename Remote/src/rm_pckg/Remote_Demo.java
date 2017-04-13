21d8e5abf71d3abbd586dc1929b57ee0e81f1e53
Ekta3008 <ep00348130@techmahindra.com>
2017-04-13 16:17:09 +0530

    Commit1--Ekta

---
+++ Remote/src/rm_pckg/Remote_Demo.java
@@ -0,0 +1,11 @@
+package rm_pckg;
+
+public class Remote_Demo {
+
+	public static void main(String[] args) {
+	System.out.println("Ekta's Update1");
+	System.out.println("Ekta's Update2");
+
+	}
+
+}
